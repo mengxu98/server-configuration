73878f3cbfbd4b7e151f9c2fd2be052b345c7205
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-23 17:16:30 +0800

    Create clash.sh

---
+++ clash/clash.sh
@@ -0,0 +1,13 @@
+
+echo "123123"|sudo -S modprobe gs_usb
+# sudo iptables -F
+
+netstat -tunllp
+sudo netstat -anp|gerp "7890"
+kill -9 7840
+
+cd ~
+cd clash
+./clash
+
+#screen -S clash ~/clash/clash-linux-amd64 -d ~/clash/
