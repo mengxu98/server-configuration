ed2e1989c274f2b9238265db8e620072c1a0abda
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2023-06-23 15:30:47 +0800

    Update r-rstudio-server.sh

--- r-rstudio-server.sh
+++ r-rstudio-server.sh
@@ -18,16 +18,23 @@ sudo apt-get install libclang-dev -y
 sudo apt-get install cmake -y
 sudo apt-get install libmagick++-dev -y
 
+sudo apt --fix-broken install -y
+
 # R
 sudo apt-get update -y
 sudo apt-get install r-base r-base-dev -y
 
 # If error: Dependency is not satisfiable: libssl1.0.0|libssl1.0.2|libssl1.1
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
+# wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb # Error
+# sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb # Error
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+sudo dpkg -i  libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
 rm libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
 
+sudo apt --fix-broken install -y
+
 # RStudio Server
+sudo apt-get update
 sudo apt-get install gdebi-core -y
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2023.03.0-386-amd64.deb
 sudo gdebi rstudio-server-2023.03.0-386-amd64.deb -n
