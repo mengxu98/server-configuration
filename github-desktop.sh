0a6dcd5522088c691c9d20cc446825e9b38b63aa
Meng xu <86040828+mengxu98@users.noreply.github.com>
2024-03-14 00:00:38 +0800

    Update github-desktop.sh

--- github-desktop.sh
+++ github-desktop.sh
@@ -5,5 +5,10 @@ sudo apt upgrade
 sudo apt install wget apt-transport-https gnupg2 software-properties-common
 
 # https://github.com/shiftkey/desktop
-wget https://github.com/shiftkey/desktop/releases/download/release-3.2.5-linux1/GitHubDesktop-linux-3.1.7-linux1.deb
-sudo apt install -f ./GitHubDesktop-linux-3.2.5-linux1.deb
+echo "Downloading GitHubDesktop."
+wget https://github.com/shiftkey/desktop/releases/download/release-3.3.8-linux2/GitHubDesktop-linux-amd64-3.3.8-linux2.deb
+
+echo "Insatlling GitHubDesktop."
+sudo apt install -f ./GitHubDesktop-linux-amd64-3.3.8-linux2.deb
+
+echo "Insatll completed."
